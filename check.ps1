0711e0f8224bc99efff8115fd55e6f4f4181228d
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-11-12 03:23:07 +0400

    1

--- check.ps1
+++ check.ps1
@@ -274,11 +274,6 @@ function ConvertTo-ActionEscapedProperty {
     Replace(',', '%2C')
 }
 
-for ($i = 1; $i -le 100; $i++ ) {
-    Write-Progress -Activity "Search in Progress" -Status "$i% Complete:" -PercentComplete $i
-    Start-Sleep -Milliseconds 250
-}
-
 Write-Information "infa sotka"
 Write-Warning "waning bla"
 
