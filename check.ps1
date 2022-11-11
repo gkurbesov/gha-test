5629d0a5fb64e32db2479bce79e4f2636822b2db
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-11-12 00:57:23 +0400

    test1

--- check.ps1
+++ check.ps1
@@ -16,9 +16,11 @@ $reff = Select-Xml -Path "Product\DirectCrm\DirectCrm.Core\DirectCrm.Core.Model.
 $reff | ForEach {[PSCustomObject]$_} | Format-Table -AutoSize
 #>
 
-Write-Host "Test write from script"
-
+Write-Host "Test from arg:"
 Write-Host $env:TEMPD
+Write-Host "Test from env: "
+Write-Host $env:GITHUB_WORKSPACE
+
 
 $files = Get-ChildItem -Path (Join-Path $PSScriptRoot "*.csproj") -Recurse
 
