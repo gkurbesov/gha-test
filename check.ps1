92f21e63259a9ef86446f648f7e0a58a3445dc53
Геннадий Курбесов <kurbesov@mindbox.ru>
2022-11-12 13:40:18 +0400

    update

--- check.ps1
+++ check.ps1
@@ -28,293 +28,56 @@ foreach ($item in $files) {
 }
 #>
 
-function Send-ActionCommand {
+Import-Module $PSScriptRoot/lib/GitHubActionsCore
+
+function Get-ProjectFiles {
     [CmdletBinding()]
-    param(
-        [Parameter(Position = 0, Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [string]$Command,
-
-        [Parameter(ParameterSetName = "WithProps", Position = 1, Mandatory)]
-        [System.Collections.IDictionary]$Properties,
-
-        [Parameter(ParameterSetName = "WithProps", Position = 2)]
-        [Parameter(ParameterSetName = "SkipProps", Position = 1)]
-        [string]$Message = ''
-    )
-
-    $cmdStr = ConvertTo-ActionCommandString $Command $Properties $Message
-    Write-Host $cmdStr
-}
-
-function Write-ActionDebug {
-    [CmdletBinding()]
-    param(
+    param (
         [Parameter(Position = 0)]
-        [string]$Message = ""
+        [string] $Path
     )
-
-    Send-ActionCommand debug $Message
+    
+    return Get-ChildItem -Path $Path -Filter "*.csproj" -Recurse
 }
 
-function Set-ActionFailed {
+function Get-PackageReferences {
     [CmdletBinding()]
-    param(
+    param (
         [Parameter(Position = 0)]
-        [string]$Message = ""
+        [string] $Path
     )
-    Write-ActionError $Message
-    #exit 1
+    
+    return Select-Xml -Path $Path -XPath "//PackageReference" | ForEach-Object { $_.node.Include }
 }
 
-function Write-ActionError {
+function Get-ProjectReferences {
     [CmdletBinding()]
-    param(
-        [Parameter(Position = 0, ParameterSetName = 'MsgOnly')]
-        [Parameter(Position = 0, ParameterSetName = 'File')]
-        [Parameter(Position = 0, ParameterSetName = 'Line')]
-        [Parameter(Position = 0, ParameterSetName = 'Column')]
-        [string]$Message = "",
-
-        [Parameter(Position = 1, ParameterSetName = 'File', Mandatory)]
-        [Parameter(Position = 1, ParameterSetName = 'Line', Mandatory)]
-        [Parameter(Position = 1, ParameterSetName = 'Column', Mandatory)]
-        [string]$File,
-
-        [Parameter(Position = 2, ParameterSetName = 'Line', Mandatory)]
-        [Parameter(Position = 2, ParameterSetName = 'Column', Mandatory)]
-        [int]$Line,
-
-        [Parameter(Position = 3, ParameterSetName = 'Column', Mandatory)]
-        [int]$Column
-    )
-    $params = [ordered]@{ }
-    if ($File) {
-        $params['file'] = $File
-    }
-    if ($PSCmdlet.ParameterSetName -in 'Column', 'Line') {
-        $params['line'] = $Line
-    }
-    if ($PSCmdlet.ParameterSetName -eq 'Column') {
-        $params['col'] = $Column
-    }
-    Send-ActionCommand error $params -Message $Message
-}
-
-function Write-ActionWarning {
-    [CmdletBinding()]
-    param(
-        [Parameter(Position = 0, ParameterSetName = 'MsgOnly')]
-        [Parameter(Position = 0, ParameterSetName = 'File')]
-        [Parameter(Position = 0, ParameterSetName = 'Line')]
-        [Parameter(Position = 0, ParameterSetName = 'Column')]
-        [string]$Message = "",
-
-        [Parameter(Position = 1, ParameterSetName = 'File', Mandatory)]
-        [Parameter(Position = 1, ParameterSetName = 'Line', Mandatory)]
-        [Parameter(Position = 1, ParameterSetName = 'Column', Mandatory)]
-        [string]$File,
-
-        [Parameter(Position = 2, ParameterSetName = 'Line', Mandatory)]
-        [Parameter(Position = 2, ParameterSetName = 'Column', Mandatory)]
-        [int]$Line,
-
-        [Parameter(Position = 3, ParameterSetName = 'Column', Mandatory)]
-        [int]$Column
-    )
-    $params = [ordered]@{ }
-    if ($File) {
-        $params['file'] = $File
-    }
-    if ($PSCmdlet.ParameterSetName -in 'Column', 'Line') {
-        $params['line'] = $Line
-    }
-    if ($PSCmdlet.ParameterSetName -eq 'Column') {
-        $params['col'] = $Column
-    }
-    Send-ActionCommand warning $params -Message $Message
-}
-
-function Write-ActionNotice {
-    [CmdletBinding()]
-    param(
-        [Parameter(Position = 0, ParameterSetName = 'MsgOnly')]
-        [Parameter(Position = 0, ParameterSetName = 'File')]
-        [Parameter(Position = 0, ParameterSetName = 'Line')]
-        [Parameter(Position = 0, ParameterSetName = 'Column')]
-        [string]$Message = "",
-
-        [Parameter(Position = 1, ParameterSetName = 'File', Mandatory)]
-        [Parameter(Position = 1, ParameterSetName = 'Line', Mandatory)]
-        [Parameter(Position = 1, ParameterSetName = 'Column', Mandatory)]
-        [string]$File,
-
-        [Parameter(Position = 2, ParameterSetName = 'Line', Mandatory)]
-        [Parameter(Position = 2, ParameterSetName = 'Column', Mandatory)]
-        [int]$Line,
-
-        [Parameter(Position = 3, ParameterSetName = 'Column', Mandatory)]
-        [int]$Column
-    )
-    $params = [ordered]@{ }
-    if ($File) {
-        $params['file'] = $File
-    }
-    if ($PSCmdlet.ParameterSetName -in 'Column', 'Line') {
-        $params['line'] = $Line
-    }
-    if ($PSCmdlet.ParameterSetName -eq 'Column') {
-        $params['col'] = $Column
-    }
-    Send-ActionCommand notice $params -Message $Message
-}
-
-
-
-## Used to signal output that is a command to Action/Workflow context
-if (-not (Get-Variable -Scope Script -Name CMD_STRING -ErrorAction SilentlyContinue)) {
-    Set-Variable -Scope Script -Option Constant -Name CMD_STRING -Value '::'
-}
-
-function ConvertTo-ActionCommandString {
-    [CmdletBinding()]
-    [OutputType([string])]
-    param(
-        [Parameter(Position = 0, Mandatory)]
-        [string]$Command,
-
-        [Parameter(Position = 1)]
-        [System.Collections.IDictionary]$Properties,
-
-        [Parameter(Position = 2)]
-        [AllowNull()]
-        [AllowEmptyString()]
-        [AllowEmptyCollection()]
-        [object]$Message
+    param (
+        [Parameter(Position = 0)]
+        [string] $Path
     )
 
-    if (-not $Command) {
-        $Command = 'missing.command'
-    }
-
-    $cmdStr = "$($CMD_STRING)$($Command)"
-    if ($Properties.Count -gt 0) {
-        $first = $true
-        foreach ($key in $Properties.Keys) {
-            $val = ConvertTo-ActionEscapedProperty $Properties[$key]
-            if ($val) {
-                if ($first) {
-                    $first = $false
-                    $cmdStr += ' '
-                }
-                else {
-                    $cmdStr += ','
-                }
-                $cmdStr += "$($key)=$($val)"
-            }
-        }
-    }
-    $cmdStr += $CMD_STRING
-    $cmdStr += ConvertTo-ActionEscapedData $Message
-
-    return $cmdStr
+    return Select-Xml -Path $Path -XPath "//ProjectReference" | ForEach-Object { Split-Path $_.node.Include -leaf }
 }
 
-function ConvertTo-ActionKeyValueFileCommand {
-    [CmdletBinding()]
-    [OutputType([string])]
-    param(
-        [Parameter(Mandatory, Position = 0)]
-        [string]$Name,
 
-        [Parameter(Mandatory, Position = 1)]
-        [AllowNull()]
-        [AllowEmptyString()]
-        [AllowEmptyCollection()]
-        [object]$Value
-    )
-    $convertedValue = ConvertTo-ActionCommandValue $Value
-    if ($convertedValue -notmatch '\n') {
-        return "$Name=$convertedValue"
-    }
-    $delimiter = "ghadelimiter_$(New-Guid)"
-    if ($Name -contains $delimiter) {
-        throw "Unexpected input: name should not contain the delimiter `"$delimiter`""
-    }
-    if ($convertedValue -contains $delimiter) {
-        throw "Unexpected input: value should not contain the delimiter `"$delimiter`""
-    }
-    $eol = [System.Environment]::NewLine
-    return "$Name<<$delimiter$eol$convertedValue$eol$delimiter"
+$ProjectFiles = Get-ProjectFiles $env:GITHUB_WORKSPACE
+
+if($ProjectFiles.Length -eq 0) {
+    Write-Warning "Project files not found!"
+    exit 1
 }
 
-<#
-.SYNOPSIS
-Sanitizes an input into a string so it can be passed into issueCommand safely.
-Equivalent of `core.toCommandValue(input)`.
-.PARAMETER Value
-Input to sanitize into a string.
-#>
-function ConvertTo-ActionCommandValue {
-    [CmdletBinding()]
-    [OutputType([string])]
-    param(
-        [Parameter(Mandatory, Position = 0)]
-        [AllowNull()]
-        [AllowEmptyString()]
-        [AllowEmptyCollection()]
-        [object]$Value
-    )
-    if ($null -eq $Value) {
-        return ''
-    }
-    if ($Value -is [string]) {
-        return $Value
-    }
-    return ConvertTo-Json $Value -Depth 100 -Compress -EscapeHandling EscapeNonAscii
+foreach ($ProjectFile in $ProjectFiles) {
+    $file = $ProjectFile
+    $project = $ProjectFile.BaseName
+
+    $PackagesReferences = Get-PackageReferences $file
+    $ProjectReferences = Get-ProjectReferences $file
+
+    Write-Host "`e[32mProject: $project"
+    Write-Host "`tPackages:"
+    $PackagesReferences | ForEach-Object { Write-Host "`t`t" $_ }
+    Write-Host "`tProjects:"
+    $ProjectReferences | ForEach-Object { Write-Host "`t`t" $_ }
 }
-
-## Escaping based on https://github.com/actions/toolkit/blob/3e40dd39cc56303a2451f5b175068dbefdc11c18/packages/core/src/command.ts#L92-L105
-function ConvertTo-ActionEscapedData {
-    [CmdletBinding()]
-    [OutputType([string])]
-    param(
-        [Parameter(Mandatory, Position = 0)]
-        [AllowNull()]
-        [AllowEmptyString()]
-        [AllowEmptyCollection()]
-        [object]$Value
-    )
-    return (ConvertTo-ActionCommandValue $Value).
-    Replace("%", '%25').
-    Replace("`r", '%0D').
-    Replace("`n", '%0A')
-}
-
-function ConvertTo-ActionEscapedProperty {
-    [CmdletBinding()]
-    [OutputType([string])]
-    param(
-        [Parameter(Mandatory, Position = 0)]
-        [AllowNull()]
-        [AllowEmptyString()]
-        [AllowEmptyCollection()]
-        [object]$Value
-    )
-    return (ConvertTo-ActionCommandValue $Value).
-    Replace("%", '%25').
-    Replace("`r", '%0D').
-    Replace("`n", '%0A').
-    Replace(':', '%3A').
-    Replace(',', '%2C')
-}
-
-Write-Information "`e[38;5;6minfa sotka"
-Write-Warning "`e[38;5;6mwaning bla"
-
-Write-ActionNotice "`e[38;5;6mNotice text"
-Write-ActionWarning "Write warning"
-Write-ActionError "Write error"
-
-
-Set-ActionFailed "Failed fuck!"
